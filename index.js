d48c7809a28a3030ebeee56b877f9ebe0fc78407
ChlorinatedPastries <chlorinatedpastries@proton.me>
2025-04-19 21:12:33 -0500

    A

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let ore = 0
-
+var ore = 0;
 
 function mine(){
-    ore+1
-    
+    var ore = 0;
+ var oree = ore + 1;
+ 
+    var ore = oree
+    console.log(ore + "a")
+    console.log(oree + "b")
 }
 
 function invin1(){
-    console.log(ore)
+    console.log(ore);
 }
